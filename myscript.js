59a0e3d6b9fb28b20c91864f518e048d09b38b26
Alex Protogerellis <alex.covve@gmail.com>
2014-11-28 11:13:34 +0200

    Analytics

---
+++ myscript.js
@@ -0,0 +1,14 @@
+﻿function clickedKindle()
+{
+ga('send', 'event', 'clickedKindle', 'click');
+}
+
+function clickedColor()
+{
+ga('send', 'event', 'clickedColor', 'click');
+}
+
+function clickedBW()
+{
+ga('send', 'event', 'clickedBW', 'click');
+}
