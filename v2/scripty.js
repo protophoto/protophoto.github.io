4a0e82ee2412d5806d39c61eb226b03053b3ae3d
ThePhantomAngel <bouzbiproto@gmail.com>
2015-11-24 01:33:02 +0200

    made v2

---
+++ v2/scripty.js
@@ -0,0 +1,22 @@
+function triggerbox() {
+document.getElementById('lightbox').style.opacity='1';
+document.getElementById('lightbox').style.visibility='visible';
+document.getElementById('thex').style.opacity='1';
+document.getElementById('thex').style.visibility='visible';
+document.getElementById('fade').style.opacity='0.8';
+document.getElementById('fade').style.display='block';
+}
+
+function closelightbox() {
+document.getElementById('lightbox').style.opacity='0';
+document.getElementById('lightbox').style.visibility='hidden';
+document.getElementById('thex').style.opacity='0';
+document.getElementById('thex').style.visibility='hidden';
+document.getElementById('fade').style.opacity='0';
+document.getElementById('fade').style.display='none';
+}
+
+function clickedColor()
+{
+ga('send', 'event', 'clickedv2', 'click');
+}
