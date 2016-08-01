da914bd5548205b95a039ab0e2ebf9002df9c7ee
protocrash <aproto.ecom@gmail.com>
2016-08-01 17:13:19 +0300

    added fbook remarketing

--- myscript.js
+++ myscript.js
@@ -1,14 +1,17 @@
 ﻿function clickedKindle()
 {
 ga('send', 'event', 'clickedKindle', 'click');
+fbq('track', 'Lead');
 }
 
 function clickedColor()
 {
 ga('send', 'event', 'clickedColor', 'click');
+fbq('track', 'Lead');
 }
 
 function clickedBW()
 {
 ga('send', 'event', 'clickedBW', 'click');
+fbq('track', 'Lead');
 }
